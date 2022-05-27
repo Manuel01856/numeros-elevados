f38a250e85ab23ecf34d0e7dbc53239b29438c8e
Manuel01856 <l21211932@tectijuana.edu.mx>
2022-05-27 11:25:03 -0700

    version 1

---
+++ numeros_elevados/numeros_elevados/Program.cs
@@ -0,0 +1,32 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace numeros_elevados
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            try
+            {
+                Console.Write("Introduzca el número que desea elevar al cuadrado: ");
+                int num = int.Parse(Console.ReadLine());
+
+                Console.WriteLine(num * num);
+                Console.ReadLine();
+            }
+            catch (DivideByZeroException e)
+            {
+                Console.Write(e.Message);
+            }
+            catch (FormatException e)
+            {
+                Console.Write(e.Message);
+            }
+            Console.ReadKey();
+        }
+    }
+}
